ebe34d7a5aab46c7326ab2ca5a9954958782dada
Ricardo Yuji <yujitsum@gmail.com>
2025-03-31 23:49:03 +0000

    recebendo o valor da porcentagem ao clicar no botão

--- main.js
+++ main.js
@@ -2,3 +2,28 @@ let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
 let buttonSelected = null
+
+//receber valor da conta 
+let billInput = document.querySelector("#bill")
+billInput.addEventListener('input', receiveBillValue)
+
+function receiveBillValue(){
+    bill = billInput.valueAsNumber
+}
+
+//receber o numero de pessoas
+
+let numberOfPeopleInput = document.querySelector ("#people")
+numberOfPeopleInput.addEventListener('input', receiveNumberOfPeople )
+
+function receiveNumberOfPeople(){
+    numberOfPeople  = numberOfPeopleInput.valueAsNumber
+    calculate()
+}
+
+function receiveTipPercentage (value){
+
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+    }
+}
