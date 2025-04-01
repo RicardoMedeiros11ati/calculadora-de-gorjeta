9adff9c18d6d8feaef5f5227ab2bb8a808bb7d58
Ricardo Yuji <yujitsum@gmail.com>
2025-04-01 00:36:41 +0000

    criação da função para calcular todos os resultados

--- main.js
+++ main.js
@@ -26,4 +26,39 @@ function receiveTipPercentage (value){
     if (buttonSelected !== null){
         buttonSelected.classList.remove("button-selected")
     }
+
+  buttonSelected = document.querySelector (`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+    tipPercentage = value / 100
+    
+    let customTipInput = document.querySelector("#custom-tip")
+    customTipInput.value = ''
+
+    calculate()
+}
+
+function receiveCustomTipPercentage (){
+    let customTipInput = document.querySelector("#custom-tip")
+    tipPercentage = customTipInput.valueAsNumber / 100
+   removeButtonSelected()
+    calculate()
+}
+
+
+function calculate(){
+
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+
+       let  tipAmountStrong = document.querySelector(".amount strong")
+       let tipAmountPerson = (bill *  tipPercentage) / numberOfPeople
+       tipAmountStrong.innerText = `$${ tipAmountPerson.toFixed(2)}`
+
+       let totalStrong = document.querySelector(".total strong")
+       let total = (bill / numberOfPeople) + tipAmountPerson
+
+       totalStrong.innerText = `$${total.toFixed(2)}`
+
+    } else {
+        console.log ("Ainda não é possível calcular")
+    }
 }
